2923819b00614b78603dfbe3ae81c083867b2e33
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-10-24 13:38:41 +0200

    Removed Spring Imports
    Signed-Off-By: Karim Jonas Groeller <groeller@kekx.eu>

--- src/main/java/eu/kekx/loomsciencepaper/LoomSciencePaperApplication.java
+++ src/main/java/eu/kekx/loomsciencepaper/LoomSciencePaperApplication.java
@@ -1,13 +1,10 @@
 package eu.kekx.loomsciencepaper;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class LoomSciencePaperApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LoomSciencePaperApplication.class, args);
+
+
     }
 
 }
